0375d9e7f6f8bcd46ecb7ac332c779435b094780
angsclash <112526057+angsclash@users.noreply.github.com>
2022-09-02 14:19:41 +0700

    Add files via upload

---
+++ python4 - tkj4.py
@@ -0,0 +1,54 @@
+'''bangun datar
+    - persegi
+    - persegi panajng
+    - trapesium
+    - segitiga
+    - jajar genjang
+    - lingkaran
+'''
+
+# rumus persegi sisi * sisi
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("luas persegi adalah", luas_p)
+
+# rumus persegi panjang * lebar
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah", luas_pp)
+
+# rumus trapesium 1/2 * (a+b) * tinggi
+
+a = 6
+b = 10
+tinggi = 8
+luas_t = (a+b) * tinggi / 2
+print("luas trapesium adalah", luas_t)
+
+# rumus segitiga 1/2 * a * t
+
+a = 10
+t = 20
+luas_s = a * t / 2
+print("luas segitiga adalah", luas_s)
+
+# rumus jajar genjang a * t
+
+a = 1000000
+t = 40
+luas_j = a * t
+print("luas jajar genjang adalah", luas_j)
+
+# rumus lingkaran 3.14 * r
+
+r = 100
+
+luas_l = 3.14 * r
+print("luas lingkaran adalah",luas_l)
+
+
+
