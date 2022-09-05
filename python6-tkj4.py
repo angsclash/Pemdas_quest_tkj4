9f43ecdbd22c25013bff4d1cb492dbc267131d57
angsclash <112526057+angsclash@users.noreply.github.com>
2022-09-05 09:54:20 +0700

    Add files via upload

---
+++ python6-tkj4.py
@@ -0,0 +1,22 @@
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+print(list_buah[1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[0])
+print(list_buah[1])
+
+print(list_buah)
+
+print(list_buah[0:1])
+print(list_buah[0:2])
+print(list_buah[1:3])
+print(list_buah[0:-1])
+print(list_buah[-1:-3])
+print(list_buah[-1:3])
+print(list_buah[-3:-1])
+
+list_buah[1:3] = ['Naga', 'Pepaya']
+
+print(list_buah)
+list_buah.insert(0, 'Jambu')
+print(list_buah)
