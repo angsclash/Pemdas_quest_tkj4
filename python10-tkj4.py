7a7d3e6709db7576b0302913ade7b568d5a0f105
angsclash <112526057+angsclash@users.noreply.github.com>
2022-10-10 09:46:22 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,23 @@
+# membuat tipe data list
+
+koleksi_data_str = ["pisang","mangga","pepaya","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["nasir", 100, True, "fuad"]
+
+print("koleksi data string:",koleksi_data_str)
+
+print("koleksi data integer:",koleksi_data_int)
+
+print("koleksi data mix:",koleksi_data_mix)
+
+# buatlah 3 kumpulan data : nama hewan, nilai uts, teman sebangku
+
+nama_hewan = ["monyed", "domge", "icikiwir", "blue lobster"]
+nilai_uts = [100, 85, 70, 99, 20]
+nama_teman_wibu = ["fajeri python", "ripeki cefot", "ilal imreng", "rafi baskath"]
+
+print("nama nama hewan yang kemce:",nama_hewan)
+print("nilai nilai uts yang gegeh:",nilai_uts)
+print("nama temen temen gweh yang wibuh pencoly handal:",nama_teman_wibu)
