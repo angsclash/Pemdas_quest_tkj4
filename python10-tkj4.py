c9df72b7b18c366a4c7f2798a0682b93039ef1b3
angsclash <112526057+angsclash@users.noreply.github.com>
2022-10-12 14:04:58 +0700

    Add files via upload

--- python10-tkj4.py
+++ python10-tkj4.py
@@ -21,3 +21,32 @@ nama_teman_wibu = ["fajeri python", "ripeki cefot", "ilal imreng", "rafi baskath
 print("nama nama hewan yang kemce:",nama_hewan)
 print("nilai nilai uts yang gegeh:",nilai_uts)
 print("nama temen temen gweh yang wibuh pencoly handal:",nama_teman_wibu)
+
+
+# data ke 2 nama hewan
+
+print("nama hewan data ke-2:", nama_hewan[1])
+
+# data pertama nilai uts
+
+print("data pertama nilai uts:", nilai_uts[0])
+
+# data terakhir nama teman sebangku
+
+print("data terakhir nama teman sebangku:", nama_teman_wibu[3])
+
+
+# tambahkan 1 data disetiap data koleksi
+
+nama_hewan.append("semut")
+nilai_uts.append(55)
+nama_teman_wibu.append("selamet")
+print(nama_hewan, nilai_uts, nama_teman_wibu)
+# ubahlah data terakhir nilai uts
+
+nilai_uts[-1] = 69
+print(nilai_uts)
+# ubahlah data ke-3 nama hewan
+
+nama_hewan[-2] = "burung"
+print(nama_hewan)
