73e53c3fa3dca31c629eb3877edee31a0deea3d2
angsclash <112526057+angsclash@users.noreply.github.com>
2022-10-18 09:51:22 +0700

    Add files via upload

---
+++ Python11-tkj4.py
@@ -0,0 +1,16 @@
+# general
+gender = ("laki-laki","perempuan")
+# menggunakan fungsi tuple, tuple()
+tuple_name =tuple(["jayabaya","james"])
+#tuple tunggal
+tuple_tunggal=("tomas")
+
+gabungan = gender + tuple_name
+
+print(gabungan)
+
+# buatlah 3 koleksi tuple dengan minimal 4 data
+
+tuple_hewan =tuple(["kucing","anjing","lobster","kambeng"])
+tuple_buah =tuple(["semangka","pisang","melon","jeruk"])
+tuple_nama =tuple(["bayu","ahmad","farhan","simanjuntak"])
